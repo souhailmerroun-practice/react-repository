9b937cd7b3d00ef3610a925a960bb71bb2f15508
Souhail M <souhail.merroun1996@gmail.com>
2022-03-14 13:22:41 +0100

    Counter using useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { useReducer } from 'react'
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      //
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
     </div>
   );
 }
