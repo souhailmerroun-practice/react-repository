fdab8a25ccfc1c11c5ed0d555ef9531b61d0d6d7
Souhail M <souhail.merroun1996@gmail.com>
2022-03-14 13:38:43 +0100

    Themed button using useContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import React, { useContext } from 'react';
+
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const defaultValue = themes.dark;
+const ThemeContext = React.createContext(defaultValue);
+
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext);
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeContext.Provider value={themes.light}>
+      <Toolbar />
+    </ThemeContext.Provider>
   );
 }
 
