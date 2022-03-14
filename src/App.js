2f8820a958a47669c138ffcefb1caba0dcf8e235
Souhail M <souhail.merroun1996@gmail.com>
2022-03-14 13:04:44 +0100

    Click me count useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      //
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
