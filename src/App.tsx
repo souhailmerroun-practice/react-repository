b720e35ee502ab8f5a68902294ffad8bb0b4787b
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-21 20:58:29 +0200

    Display countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface Countdown {
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+}
+
 function App() {
+
+  const countDownDate = new Date("May 23, 2022 15:37:25").getTime();
+  const now = new Date().getTime(); // Get today's date and time
+
+  const [countdown, setCountdown] = useState<Countdown>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log('hi');
+
+      // Find the distance between now and the count down date
+      const distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setCountdown({
+        days,
+        hours,
+        minutes,
+        seconds
+      })
     }, 1000);
 
     return () => clearInterval(interval);
@@ -15,18 +47,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {countdown.days}d {countdown.hours}h {countdown.minutes}m {countdown.seconds}s
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
