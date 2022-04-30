1632ab0efe4c4376682f362f3fd7dfc6aa8ef672
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-05-01 00:23:55 +0200

    Setup forms

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
     return (
         <div>
             <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label>Password</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button>Login</button>
+            </form>
         </div>
     );
 }
