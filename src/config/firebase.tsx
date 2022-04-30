c8bda7fa9ba457ca31a856eb7987acda13fd86d5
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-05-01 00:10:08 +0200

    config/firebase.tsx

---
+++ src/config/firebase.tsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAnk8tx9EMtd9vOYke1W0lxWCYBjlajHLM",
+    authDomain: "authentification-firebas-44162.firebaseapp.com",
+    projectId: "authentification-firebas-44162",
+    storageBucket: "authentification-firebas-44162.appspot.com",
+    messagingSenderId: "716533293149",
+    appId: "1:716533293149:web:418ef4279040b572a82db4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
